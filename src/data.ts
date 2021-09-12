5538e4e2e42bf90abf98b2ebe17e93dfcedbf248
MHammadYou <MHammadY2.0@gmail.com>
2021-09-12 22:51:25 +0500

    Called chess.com api

--- src/data.ts
+++ src/data.ts
@@ -1,1 +1,10 @@
 import axios from "axios";
+
+
+axios.get('https://api.chess.com/pub/player/erik')
+  .then(response => {
+    console.log(response);
+  })
+  .catch(error => {
+    console.log(error);
+  })
