06eaf028872354beaa290c4f0a750c8c18e0b213
MHammadYou <MHammadY2.0@gmail.com>
2021-09-13 22:47:57 +0500

    Added Pgn call route

--- src/data.ts
+++ src/data.ts
@@ -1,10 +1,24 @@
 import axios from "axios";
 
+let archive = null;
 
-axios.get('https://api.chess.com/pub/player/erik')
-  .then(response => {
-    console.log(response);
-  })
-  .catch(error => {
-    console.log(error);
-  })
+
+export default function getData() {
+  axios.get('https://api.chess.com/pub/player/erik/games/archives')
+    .then(response => {
+      const archives = response.data.archives;
+      archive = archives[0];
+
+      axios.get(archive)
+        .then(response => {
+          console.log(response.data.games[0].pgn)
+          return response.data.games[0].pgn;
+        })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-import "./data";
+import getData from "./data"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  return res.json({"msg": "Working"})
+app.get('/', async (req, res) => {
+
+  const data = await getData();
+  return res.json(data);
+
 })
 
 
