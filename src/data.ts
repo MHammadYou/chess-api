8f3ee90ce559470cdf8d470a8b4dae1d75ccaab0
MHammadYou <MHammadY2.0@gmail.com>
2021-09-14 22:32:27 +0500

    Added routes

--- src/data.ts
+++ src/data.ts
@@ -1,18 +1,16 @@
 import axios from "axios";
 
-let archive = null;
-
 
 export default function getData() {
   axios.get('https://api.chess.com/pub/player/erik/games/archives')
     .then(response => {
       const archives = response.data.archives;
-      archive = archives[0];
+      const archive = archives[0];
 
       axios.get(archive)
-        .then(response => {
-          console.log(response.data.games[0].pgn)
-          return response.data.games[0].pgn;
+        .then(res => {
+          const _data = res.data.games[0].pgn;
+          return _data;
         })
     })
     .catch(error => {
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
-import getData from "./data"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', async (req, res) => {
+app.use(express.json());
 
-  const data = await getData();
-  return res.json(data);
+import indexRoute from "./routes/index";
 
-})
+app.use('/', indexRoute);
 
 
 app.listen(PORT, () => `Listening on port: ${PORT}`);
