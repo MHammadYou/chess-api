e7de36337de99d801635f27be22e232793cbde71
MHammadYou <MHammadY2.0@gmail.com>
2021-09-16 22:41:33 +0500

    Added random player profile with url param

--- src/routes/random-match.ts
+++ src/routes/random-match.ts
@@ -1,11 +1,24 @@
 import express from "express";
+import axios from "axios";
 
 const router = express.Router();
 
 
 router.get('/random/:player',  (req, res) => {
-  const player: string = req.params.player;
-  res.json({"Player": player})
+
+  const username = req.params.player;
+
+  axios.get(`https://api.chess.com/pub/player/${username}/`)
+    .then(response => {
+      const data = response.data;
+      res.json(data);
+    })
+    .then(error => {
+      console.log("hit")
+      console.log(error);
+      res.send("Not found");
+    })
+
 })
 
 
