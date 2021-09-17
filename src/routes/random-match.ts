33752c480aafb5c9197448dbd242354be7cf7038
MHammadYou <MHammadY2.0@gmail.com>
2021-09-17 23:48:36 +0500

    Random match route now returns match pgn

--- src/routes/random-match.ts
+++ src/routes/random-match.ts
@@ -4,22 +4,37 @@ import axios from "axios";
 const router = express.Router();
 
 
+interface MatchData {
+  pgn: string
+}
+
+
 router.get('/random/:player',  (req, res) => {
 
   const username = req.params.player;
+  const url = `https://api.chess.com/pub/player/${username}/games/2007/07`;
 
-  axios.get(`https://api.chess.com/pub/player/${username}/`)
+
+  axios.get(url)
+
     .then(response => {
+
       const data = response.data;
-      res.json(data);
+      const pgnSplitArr = data.games[0].pgn.split('\n');
+      const pgn = pgnSplitArr[pgnSplitArr.length - 2];
+
+      const matchResult: MatchData = {
+        pgn,
+      }
+
+      res.json(matchResult);
+
     })
-    .then(error => {
-      console.log("hit")
+
+    .catch(error => {
       console.log(error);
-      res.send("Not found");
     })
 
 })
-
 
 export default router;
